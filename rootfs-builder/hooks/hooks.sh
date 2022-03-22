c6cd143c0466267685c5add15f1a12f90fa87bac
dml <dml>
2022-03-22 14:52:31 +0800

    添加hooks

---
+++ rootfs-builder/hooks/hooks.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+add_repo(){
+  echo "deb [trusted=yes]  http://10.2.10.31/repo/project/pms947-htwy/ eagle main" >> /etc/apt/sources.list
+  apt update
+}
+install_pkg(){
+  apt install -y grub-common initramfs-tools-core live-tools parted  gcc g++ sudo 
+  apt install -y linux-headers-3.10.0-mips64-core-947=3.10.0-1005 linux-image-3.10.0-mips64-core-947=3.10.0-1005 \
+          default-jdk openssh-server openssh-client sqlite vsftpd mariadb-common  mariadb-server mariadb-client  
+}
+update_fstab(){
+echo "# UNCONFIGURED FSTAB FOR BASE SYSTEM
+/dev/sda1 / ext3 rw,relatime 0 1" > /etc/fstab
+}
+copy_grubconf(){
+  if [ -f /hooks-data/11_linux ];then
+          cp -v /hooks-data/11_linux /etc/grub.d/
+  fi
+}
+copy_bootcfg(){
+  if [ -f /hooks-data/boot.cfg ];then
+          cp -v /hooks-data/boot.cfg /boot/
+  fi
+}
+change_root_passwd(){
+  echo root:a\$|chpasswd
+}
+main(){
+  add_repo
+  install_pkg
+  update_fstab
+  copy_grubconf
+  copy_bootcfg
+  change_root_passwd
+}
+
+main
