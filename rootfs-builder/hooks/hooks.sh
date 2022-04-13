4be524d1102e2ce01c1aaf46588c61056065ca4f
ML D <mld@MLdeMacBook-Pro.local>
2022-04-13 14:41:43 +0800

    优化安装软件包,解决无法静默安装的问题

--- rootfs-builder/hooks/hooks.sh
+++ rootfs-builder/hooks/hooks.sh
@@ -4,16 +4,23 @@ add_repo(){
   echo "deb [trusted=yes]  http://10.2.10.31/repo/project/pms806-719s-2k1000/ fou main" >> /etc/apt/sources.list
   apt update
 }
+
+apt_install() {
+  DEBIAN_FRONTEND="noninteractive" apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"  --allow-unauthenticated install "$@"
+}
+
 install_pkg(){
-  apt install -yq grub-common initramfs-tools-core live-boot \
+  apt install -y grub-common initramfs-tools-core live-boot \
       live-boot-initramfs-tools openssl pciutils vim live-tools parted gcc g++ sudo
   if [ -f /package.list/extra_deb.list ] && [ -f /package.list/kernel.list ] ;then
-    xargs --arg-file=/kernel.list apt-get -yq install
-    xargs --arg-file=/extra_deb.list apt-get -yq install
+    kernel_deb=$(xargs --arg-file=/package.list/kernel.list)
+    extra_deb=$(xargs --arg-file=/package.list/extra_deb.list)
+    apt_install "$kernel_deb" "$extra_deb"
   else
-    echo "no package will install"
+    echo "no packagelist will install"
   fi
 }
+
 update_fstab(){
 echo "# UNCONFIGURED FSTAB FOR BASE SYSTEM
 /dev/sda1 / ext3 rw,relatime 0 1" > /etc/fstab
