6db9c6f7cbed3a01e43ae9d329ddf89397788629
ML D <mld@MLdeMacBook-Pro.local>
2022-04-13 16:43:37 +0800

    优化安装软件包的地方

--- rootfs-builder/hooks/hooks.sh
+++ rootfs-builder/hooks/hooks.sh
@@ -15,7 +15,8 @@ install_pkg(){
   if [ -f /package.list/extra_deb.list ] && [ -f /package.list/kernel.list ] ;then
     kernel_deb=$(xargs --arg-file=/package.list/kernel.list)
     extra_deb=$(xargs --arg-file=/package.list/extra_deb.list)
-    apt_install "$kernel_deb" "$extra_deb"
+    # 这里打死不能写成apt_install ”$kernel_deb“ ”$extra_deb“ 会被转义成带单引号的变量从而无法安装
+    apt_install $kernel_deb $extra_deb
   else
     echo "no packagelist will install"
   fi
