3971aaaf0d54a22633ba1dbd5693f3c903ca3b05
ML D <mld@MLdeMacBook-Pro.local>
2022-04-14 15:01:16 +0800

    给squashfs里整上自动登录

--- rootfs-builder/mkiso.sh
+++ rootfs-builder/mkiso.sh
@@ -44,10 +44,19 @@ mk_binary(){
         mkdir -p ./binary/live
         cp -v ./squashfs-root/boot/initrd* ./binary/boot/initrd.img
         cp -v ./squashfs-root/boot/vmlinu* ./binary/boot/vmlinuz
-        mksquashfs ./squashfs-root ./filesystem.squashfs-new
-        cp -v ./filesystem.squashfs-new ./binary/live/filesystem.squashfs
+        mksquashfs ./squashfs-root ./binary/live/filesystem.squashfs
         sync
     fi 
+}
+
+enable_autologin(){
+    chroot_path="./squashfs-root"
+   	sed -i "s|ExecStart=-/sbin/agetty|ExecStart=-/sbin/agetty --autologin root |g" "$chroot_path"/lib/systemd/system/serial-getty@.service
+    sed -i "/ExecStart/i\ExecStartPre=-/bin/stty -F /dev/%I speed 115200 -opost" "$chroot_path"/lib/systemd/system/serial-getty@.service
+    for i in {0..7}
+    do
+        ln -s /lib/systemd/system/serial-getty@.service "$chroot_path"/etc/systemd/system/getty.target.wants/serial-getty@ttyS"${i}".service
+    done
 }
 
 mk_iso(){
@@ -62,6 +71,7 @@ main(){
     un_squashfs
     copy_rootfs
     copy_install_scipts
+    enable_autologin
     mk_binary
     mk_iso
 
