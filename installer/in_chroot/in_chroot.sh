eded4876d1c943ef993971521765241aee2049cc
DML <605811524@qq.com>
2022-05-17 15:08:51 +0800

    整理环境,添加用户和时区设置脚本

--- installer/in_chroot/in_chroot.sh
+++ installer/in_chroot/in_chroot.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "=========================in_chroot==================================="
 DEVICE="$1"
+conf_path="/"
 bash /installer/setup_bootloader.sh "$DEVICE" |tee -a /var/log/installer.log
+# 
+bash /installer/set_user.sh
+bash /installer/set_timezone.sh
